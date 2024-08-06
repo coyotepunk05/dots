2e3817fa40d09181af5e4d2a08aa04759b2ab38c
coyotepunk05 <coyote582005@gmail.com>
2024-08-05 22:19:59 -0700

    lazyvim v1

--- .config/hypr/scripts/swww.sh
+++ .config/hypr/scripts/swww.sh
@@ -8,16 +8,16 @@ PICS=($(ls $DIR -I '*.json'))
 
 #echo "$PICS"
 
-RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+RANDOMPICS=${PICS[$RANDOM % ${#PICS[@]}]}
 
 swww query || swww init
 
 echo Setting ${DIR}/${RANDOMPICS}
 
-swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
-
-wal -i "${DIR}/${RANDOMPICS}" -n
+swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type grow --transition-pos 0,1600 --transition-duration 3
 
 pywalfox update
 
 pywal-discord -p ~/.config/vesktop/themes -t pywal
+
+wal -i "${DIR}/${RANDOMPICS}" -n
