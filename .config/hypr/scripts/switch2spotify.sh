e7152aa617a7a391b5a57053184d8823468ed1f4
coyotepunk05 <coyote582005@gmail.com>
2024-11-29 00:45:31 -0800

    fix immediate comical bug with script

--- .config/hypr/scripts/switch2spotify.sh
+++ .config/hypr/scripts/switch2spotify.sh
@@ -3,9 +3,11 @@
 workspace=$(hyprctl clients | grep -B 4 'class: Spotify' | grep 'workspace:' | awk '{print $2}')
 activeworkspace=$(hyprctl activeworkspace -j | jq '.id')
 
-if [ "$activeworkspace = $workspace" ]; then
-  echo same
+echo $workspace
+echo $activeworkspace
+
+if [ "$workspace" = "$activeworkspace" ]; then
   hyprctl dispatch workspace previous
 else
-  hyprctl dispatch workspace $workspace
+  hyprctl dispatch workspace "$workspace"
 fi
