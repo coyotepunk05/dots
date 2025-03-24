274a13d5bb14b13f065835e9eda1bab5639ab5ea
coyotepunk05 <coyote582005@gmail.com>
2025-03-23 21:43:27 -0700

    new plugins

--- .config/waybar/scripts/spotify.sh
+++ .config/waybar/scripts/spotify.sh
@@ -5,7 +5,7 @@ icon=""
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-  if [[ ${#info} > 40 ]]; then
+  if [[ ${#info} ]] >40; then
     info=$(echo $info | cut -c1-40)"..."
   fi
   text=$info" "$icon
