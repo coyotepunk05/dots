f03f3a30a391abb13f4a841839dac949f840da41
coyotepunk05 <coyote582005@gmail.com>
2025-05-11 18:45:28 -0700

    fixed notifcation for cancelled screenshot

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,46 +1,55 @@
 #!/usr/bin/env bash
 
 # Flags:
-
 # r: region
 # s: screen
-#
 # c: clipboard
 # f: file
 # i: interactive
-
 # p: pixel
 
+capture_region() {
+  region=$(slurp -b '#000000b0' -c '#00000000')
+  [[ $? -ne 0 ]] && exit 1
+  echo "$region"
+}
+
+timestamped_filename() {
+  mkdir -p ~/Pictures/Screenshots
+  echo ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
+}
+
 if [[ $1 == rc ]]; then
-    grim -g "$(slurp -b '#000000b0' -c '#00000000')" - | wl-copy
-    notify-send 'Copied to Clipboard' Screenshot
+  region=$(capture_region) || exit 1
+  grim -g "$region" - | wl-copy
+  notify-send 'Copied to Clipboard' Screenshot
 
 elif [[ $1 == rf ]]; then
-    mkdir -p ~/Pictures/Screenshots
-    filename=~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
-    grim -g "$(slurp -b '#000000b0' -c '#00000000')" $filename
-    wl-copy < $filename 
-    notify-send 'Screenshot Taken, Copied To Clipboard' $filename
+  region=$(capture_region) || exit 1
+  filename=$(timestamped_filename)
+  grim -g "$region" "$filename"
+  wl-copy <"$filename"
+  notify-send 'Screenshot Taken, Copied To Clipboard' "$filename"
 
 elif [[ $1 == ri ]]; then
-    grim -g "$(slurp -b '#000000b0' -c '#00000000')" - | swappy -f -
+  region=$(capture_region) || exit 1
+  grim -g "$region" - | swappy -f -
 
 elif [[ $1 == sc ]]; then
-    filename=~/Pictures/Screenshots/%Y-%m-%d_%H-%M-%S.png
-    grim - | wl-copy
-    notify-send 'Copied to Clipboard' Screenshot
+  grim - | wl-copy
+  notify-send 'Copied to Clipboard' Screenshot
 
 elif [[ $1 == sf ]]; then
-    mkdir -p ~/Pictures/Screenshots
-    filename=~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
-    grim $filename
-    notify-send 'Screenshot Taken' $filename
+  filename=$(timestamped_filename)
+  grim "$filename"
+  notify-send 'Screenshot Taken' "$filename"
 
 elif [[ $1 == si ]]; then
-    grim - | swappy -f -
+  grim - | swappy -f -
 
 elif [[ $1 == p ]]; then
-    color=$(hyprpicker -a)
-    wl-copy $color
-    notify-send 'Copied to Clipboard' $color
+  color=$(hyprpicker -a)
+  [[ $? -ne 0 ]] && exit 1
+  wl-copy "$color"
+  notify-send 'Copied to Clipboard' "$color"
 fi
