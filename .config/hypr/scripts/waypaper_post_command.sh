e2b14cabb25c8dfc73ee73c2b643e2be28470a49
coyotepunk05 <coyote582005@gmail.com>
2025-07-20 13:49:12 -0700

    hyprpolkitagent

---
+++ .config/hypr/scripts/waypaper_post_command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wallpaper="$1"
+
+wal -i "$wallpaper" -n -e
+
+sleep 0.7s
+
+swww img $wallpaper --transition-fps 60 --transition-type grow --transition-pos 0,1600 --transition-duration 3
+
+pywalfox update
+
+cp ~/.cache/wal/cava.colors ~/.config/cava/config && cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css && cp ~/.cache/wal/colors-waybar.css ~/.config/wlogout/colors-waybar.css && cp ~/.cache/wal/colors-waybar.css ~/.config/hyprsw/colors-waybar.css && cp ~/.cache/wal/colors-waybar.css ~/.config/sherlock/colors-waybar.css
+
+sh ~/.config/hypr/scripts/starshipwal/starshipwal_contrast.sh
